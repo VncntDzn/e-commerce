aa50f06579b37f78bcd123aa38e5dfabc4b14576
Vincent Dizon <vince@kampo.ph>
2021-03-24 08:52:54 +0800

    added texts to footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,32 +4,43 @@ import { Button, Typography, Box, Grid, Hidden } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
 import customTheme from 'theme/customTheme';
-import HappyPeople from './assets/community.png';
 
 const Community = (props) => {
   const useStyles = makeStyles((theme) => ({
     container: {
-      backgroundColor: customTheme.palette.tertiary.light,
+      backgroundColor: customTheme.palette.secondary.light,
+      padding: theme.spacing(3),
     },
-    containerSlogan: {
-      display: 'flex',
-      flexDirection: 'column',
-      placeContent: 'center',
-    },
-    fluid_header: {
-      fontSize: 'clamp(1.5rem, 5vw, 3rem)',
-      fontWeight: 600,
-    },
-    fluid_paragraph: {
-      fontSize: 'clamp(1rem, 4vw, 1.3rem)',
-      color: `${customTheme.palette.tertiary.dark}`,
+    fluid_footer: {
+      fontSize: 'clamp(1rem, 3vw, 1.2rem)',
+      fontWeight: 500,
+      color: '#000000',
     },
   }));
 
   const classes = useStyles();
   return (
-    <Grid container direction='row'>
-      <Box>Footer Here!</Box>
+    <Grid
+      container
+      direction='row'
+      justify='space-between'
+      alignItems='center'
+      className={classes.container}
+    >
+      <Grid container item direction='row' justify='space-around'>
+        <Typography className={classes.fluid_footer}>
+          Conditions of Use
+        </Typography>
+        <Typography className={classes.fluid_footer}>Privacy Notice</Typography>
+        <Typography className={classes.fluid_footer}>
+          Interest-Based Ads
+        </Typography>
+      </Grid>
+      <Grid container item direction='row' justify='center'>
+        <Typography className={classes.fluid_footer}>
+          ©2021. E-comm Inc, or its affiliates.
+        </Typography>
+      </Grid>
     </Grid>
   );
 };

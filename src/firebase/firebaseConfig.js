112f9d3c5655d82dff7f29bc3df47091ac2517ce
Vincent Dizon <vince@kampo.ph>
2021-03-26 14:51:07 +0800

    added firebase config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+require('dotenv');
+
+var firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

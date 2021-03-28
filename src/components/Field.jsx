b0aed371ced9ba39e8651c1c74d3282f6d1b4d01
Vincent Dizon <vince@kampo.ph>
2021-03-28 10:17:54 +0800

    added form in signup

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useField } from 'formik';
-import TextField from '@material-ui/core/TextField';
+import { TextField, Box } from '@material-ui/core';
 
 const Field = ({ ...props }) => {
   const [field, meta] = useField(props);
   return (
-    <>
+    <Box py={1}>
       <TextField fullWidth {...field} {...props} />
       {meta.touched && meta.error ? (
         <div className='error' style={{ color: 'red' }}>
           {meta.error}
         </div>
       ) : null}
-    </>
+    </Box>
   );
 };
 
--- src/pages/auth/signup/Signup.jsx
+++ src/pages/auth/signup/Signup.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { Card, CardContent, Button, Box } from '@material-ui/core';
+import { Formik, Form } from 'formik';
+import { signupSchema } from 'helpers';
+import { Field } from 'components';
+import { MainLayout } from 'layouts';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Signup = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignSelf: 'center',
+      alignItems: 'center',
+      height: '75vh',
+    },
+    cardContainer: {
+      width: '80vw',
+      height: 'fit-content',
+      [theme.breakpoints.up('sm')]: {
+        width: '30rem',
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Sign up</h1>
-    </div>
+    <MainLayout>
+      <Box className={classes.container}>
+        <Card raised className={classes.cardContainer}>
+          <CardContent>
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+              }}
+              validationSchema={signupSchema}
+              onSubmit={() => {
+                alert('nice');
+              }}
+            >
+              <Form>
+                <Field name='email' type='email' placeholder='Email' />
+                <Field name='firstName' placeholder='First Name' />
+                <Field name='lastName' placeholder='Last Name' />
+                <Field name='password' placeholder='Password' type='password' />
+                <Box mt={3}>
+                  <Button
+                    color='secondary'
+                    variant='contained'
+                    fullWidth
+                    type='submit'
+                  >
+                    Submit
+                  </Button>
+                </Box>
+              </Form>
+            </Formik>
+          </CardContent>
+        </Card>
+      </Box>
+    </MainLayout>
   );
 };
 

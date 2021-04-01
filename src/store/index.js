83db7a0eedf9e81fc2502453a5e392f4da79eec8
Vincent Dizon <vince@kampo.ph>
2021-04-01 10:09:48 +0800

    download package redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,24 @@
 
 import { configureStore } from '@reduxjs/toolkit';
+
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 import authSlice from './slices/authSlice';
 
+const reducers = combineReducers({
+    counter: authSlice,
+});
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-    reducer: authSlice
+    reducer: persistedReducer,
 
 });
 
